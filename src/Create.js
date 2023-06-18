0173a997b4b1ecb841ab32ba6c3522105813e07d
Lukman Kazeem <lukman.kazeem.9@gmail.com>
2023-06-18 05:59:23 +0100

    forms demo

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,44 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('nan');
+
+    return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Blog title</label>
+                <textarea
+                    required
+                    value={ body }
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+
+                <label>Blog author:</label>
+                <select
+                    value = {author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value='mario'>mario</option>
+                    <option value='yoshi'>yoshi</option>
+                </select>
+                <button>Add me</button>
+
+                <p>{ title }</p>
+                <p>{ body }</p>
+            </form>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
